0935ab0c33c90f13fe0b0359cfcf8a9a1345f195
fattalion <fattalion@disroot.de>
2021-03-30 13:50:08 +0300

    Use classes for engines

--- simplytranslate_engines/libretranslate.py
+++ simplytranslate_engines/libretranslate.py
@@ -1,36 +1,42 @@
 import json
 import requests
 
-supported_languages = {
-    "English": "en",
-    "Arabic": "ar",
-    "Chinese": "zh",
-    "French": "fr",
-    "German": "de",
-    "Italian": "it",
-    "Portuguese": "pt",
-    "Russian": "ru",
-    "Spanish": "es",
-}
+class LibreTranslateEngine:
+    def __init__(self, url="https://libretranslate.com"):
+        self.url = url
 
-def translate(text, to_language, from_language="auto", libretranslate_url="https://libretranslate.com/translate"):
-    r = requests.post(
-        libretranslate_url,
-        json={
-            "q": text,
-            "source": from_language,
-            "target": to_language
-        })
+    name = "libre"
 
-    #response = dict(r.text)
-    response = json.loads(r.text)
-    if "translatedText" in response:
-        return response["translatedText"]
-    elif "error" in response:
-        return response["error"]
-    else:
-        return "odd, something went wrong"
+    def get_supported_languages(self):
+        return {
+            "English": "en",
+            "Arabic": "ar",
+            "Chinese": "zh",
+            "French": "fr",
+            "German": "de",
+            "Italian": "it",
+            "Portuguese": "pt",
+            "Russian": "ru",
+            "Spanish": "es",
+        }
 
+    def translate(self, text, to_language, from_language="auto"):
+        r = requests.post(
+            f"{self.url}/translate",
+            json={
+                "q": text,
+                "source": from_language,
+                "target": to_language
+            })
+
+        #response = dict(r.text)
+        response = json.loads(r.text)
+        if "translatedText" in response:
+            return response["translatedText"]
+        elif "error" in response:
+            return response["error"]
+        else:
+            return "odd, something went wrong"
 
 if __name__ == "__main__":
-    print(translate("lutsch meinen schwanz", "en"))
+    print(LibreTranslateEngine().translate("lutsch meinen schwanz", "en"))

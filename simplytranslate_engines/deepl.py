c18f0b32a3206010d51aeb8f67eabc21f71a0198
metalune <metalune@mailbox.org>
2022-01-23 22:03:46 +0100

    Add deepl support using translatepy

--- simplytranslate_engines/deepl.py
+++ simplytranslate_engines/deepl.py
@@ -1,10 +1,11 @@
-# The code included in this file was originally inspired by https://github.com/KeKsBoTer/deepl-cli/blob/master/deepl.py
+# e code included in this file was originally inspired by https://github.com/KeKsBoTer/deepl-cli/blob/master/deepl.py
 # Sadly I could find no license file attached to this
 
 import requests
 import time
 import json
 from datetime import datetime
+from translatepy.translators.deepl import DeeplTranslate
 
 class DeeplEngine:
     name = "deepl"
@@ -13,12 +14,12 @@ class DeeplEngine:
     def __init__(self):
         self.session = requests.Session()
         self.delay_begin = None
+        self.deepl = DeeplTranslate()
 
     def get_supported_source_languages(self):
         return {"Autodetect": "auto", **self.get_supported_target_languages()}
 
-    def get_supported_target_languages(self):
-        return {
+    def get_supported_target_languages(self): return {
             "Bulgarian": "BG",
             "Chinese": "ZH",
             "Czech": "CS",
@@ -52,52 +53,8 @@ class DeeplEngine:
         return None
 
     def translate(self, text, to_language, from_language="auto"):
-        if self.delay_begin != None:
-            difference = datetime.now() - self.delay_begin
-            if difference.seconds > 120:
-                self.delay_begin = None
-                self.session = requests.Session()
-            else:
-                return "error: Too many requests, please wait a few seconds"
 
-        data = {
-            "jsonrpc": "2.0",
-            "method": "LMT_handle_jobs",
-            "params": {
-                "jobs": [
-                    {
-                        "kind": "default",
-                        "raw_en_sentence": text,
-                        "raw_en_context_before": [],
-                        "raw_en_context_after": [],
-                        "quality": "fast"
-                    }
-                ],
-                "lang": {
-                    "user_preferred_langs": [from_language, to_language],
-                    "source_lang_user_selected": from_language,
-                    "target_lang": to_language
-                },
-                "priority": -1,
-                "timestamp": int(time.time()*1000)
-            }
-        }
-        response = self.session.post("https://www2.deepl.com/jsonrpc", params={}, json=data)
-
-        j_content = json.loads(response.content)
-
-        if "error" in j_content:
-            error_message = j_content["error"]["message"]
-            if error_message == "Too many requests":
-                # Introduce an artificial delay until the next request can be made
-                # So the server doesn't end up banning our IP
-                self.delay_begin = datetime.now()
-                return "error: Too many requests, please wait a few seconds"
-
-            return f"error: {error_message}"
-
-        # There are potentially more translations attached to this request, but we ignore them for now
-        translation = j_content["result"]["translations"][0]["beams"][0]["postprocessed_sentence"]
+        translation = self.deepl.translate(text, to_language, from_language)
 
         return translation
 

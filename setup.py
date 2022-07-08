135a0dd4abba50b3d4678089a364192558a46c33
jeanluc <lkubb@protonmail.com>
2022-07-08 22:21:33 +0200

    Add explanation of version requirement for aiohttp

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setup(
         "lxml",
         "requests",
         "translatepy",
+        # Currently, pip defaults to a broken alpha release from 2019.
+        # Requiring <4 forces the installation of the correct, stable package.
+        # see https://codeberg.org/SimpleWeb/SimplyTranslate-Docker/issues/1
         "aiohttp<4",
         "aiocache"
     ]
